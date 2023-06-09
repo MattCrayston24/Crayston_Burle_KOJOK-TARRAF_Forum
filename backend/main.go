0e147b9ca36a39e90edee388a9bd81017c3c1aad
KhaledTarraf <khaled23mdjkmk@gmail.com>
2023-06-09 12:37:19 +0200

    all pages

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -19,6 +20,12 @@ type Utilisateur struct {
 	DateInscription string
 }
 
+type Topic struct {
+	ID            int
+	Titre         string
+	UtilisateurID int
+}
+
 func main() {
 	var err error
 	db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/forum")
@@ -32,6 +39,9 @@ func main() {
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/connexion", loginHandler)
+	http.HandleFunc("/programme", programHandler)
+
+	fmt.Println("(http://localhost:8080/) - Server is running on port 8080")
 
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -77,5 +87,40 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 				http.Redirect(w, r, "/", http.StatusSeeOther)
 			}
 		}
+	}
+}
+
+func programHandler(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT id, titre, utilisateur_id FROM topic")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var topics []Topic
+	for rows.Next() {
+		var t Topic
+		if err := rows.Scan(&t.ID, &t.Titre, &t.UtilisateurID); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		topics = append(topics, t)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("../front/programme.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, topics)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
