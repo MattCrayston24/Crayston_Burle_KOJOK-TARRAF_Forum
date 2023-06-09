edf159e48a5e7d7df3ebfb9157d96dc996ff2223
KhaledTarraf <khaled23mdjkmk@gmail.com>
2023-06-09 15:25:38 +0200

    modif go and rework data base

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"html/template"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,24 +10,48 @@ import (
 
 var db *sql.DB
 
+//Representation des tables en struct
+
+type Role struct {
+	ID_ROLE int
+	GRADE   string
+}
+
+type Categorie struct {
+	ID_CATEGORIE int
+	TITRE        string
+}
+
 type Utilisateur struct {
-	ID              int
-	NomUtilisateur  string
-	AdresseMail     string
-	MotDePasse      string
-	RoleID          int
-	DateInscription string
+	ID_UTILISATEUR  int
+	NOM_UTILISATEUR string
+	ADRESSE_MAIL    string
+	MOT_DE_PASSE    string
+	ID_ROLE         int
 }
 
 type Topic struct {
-	ID            int
-	Titre         string
-	UtilisateurID int
+	ID_TOPIC       int
+	TITRE          string
+	ID_UTILISATEUR int
+}
+
+type Message struct {
+	ID_MESSAGE     int
+	CONTENU        string
+	ID_MESSAGE_1   int
+	ID_TOPIC       int
+	ID_UTILISATEUR int
+}
+
+type Definir struct {
+	ID_TOPIC     int
+	ID_CATEGORIE int
 }
 
 func main() {
 	var err error
-	db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/forum")
+	db, err = sql.Open("mysql", "root@tcp(127.0.0.1:3306)/newforum")
 	if err != nil {
 		panic(err)
 	}
@@ -68,6 +91,10 @@ func produitsHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "../front/produits.html")
 }
 
+func programHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../front/programme.html")
+}
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		http.ServeFile(w, r, "../front/connexion.html")
@@ -75,15 +102,15 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 
 		email := r.FormValue("email")
-		password := r.FormValue("password")
+		password := r.FormValue("password") // Pensez à hacher ce mot de passe avant de le comparer à celui dans la base de données
 
 		var utilisateur Utilisateur
-		err := db.QueryRow("SELECT * FROM utilisateur WHERE adresse_mail = ? AND mot_de_passe = ?", email, password).Scan(&utilisateur.ID, &utilisateur.NomUtilisateur, &utilisateur.AdresseMail, &utilisateur.MotDePasse, &utilisateur.RoleID, &utilisateur.DateInscription)
+		err := db.QueryRow("SELECT * FROM UTILISATEUR WHERE ADRESSE_MAIL = ? AND MOT_DE_PASSE = ?", email, password).Scan(&utilisateur.ID_UTILISATEUR, &utilisateur.NOM_UTILISATEUR, &utilisateur.ADRESSE_MAIL, &utilisateur.MOT_DE_PASSE, &utilisateur.ID_ROLE)
 		if err != nil {
 			fmt.Fprintln(w, "Connexion échouée")
 		}
 
-		if utilisateur.AdresseMail != "" {
+		if utilisateur.ADRESSE_MAIL != "" {
 			http.Redirect(w, r, "/", http.StatusSeeOther)
 		} else {
 			fmt.Fprintln(w, "Connexion échouée")
@@ -91,51 +118,16 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
 		username := r.FormValue("username")
 		signupEmail := r.FormValue("signup-email")
-		signupPassword := r.FormValue("signup-password")
-		roleID := 2
+		signupPassword := r.FormValue("signup-password") // Vous devriez hacher ce mot de passe avant de le stocker
+		roleID := 3                                      // Selon votre exemple d'insertion, le rôle d'utilisateur normal a un ID_ROLE de 3
 
 		if username != "" && signupEmail != "" && signupPassword != "" {
-			_, err := db.Exec("INSERT INTO utilisateur (nom_utilisateur, adresse_mail, mot_de_passe, role_id) VALUES (?, ?, ?, ?)", username, signupEmail, signupPassword, roleID)
+			_, err := db.Exec("INSERT INTO UTILISATEUR (NOM_UTILISATEUR, ADRESSE_MAIL, MOT_DE_PASSE, ID_ROLE) VALUES (?, ?, ?, ?)", username, signupEmail, signupPassword, roleID)
 			if err != nil {
 				fmt.Fprintln(w, "Inscription échouée")
 			} else {
 				http.Redirect(w, r, "/", http.StatusSeeOther)
 			}
 		}
-	}
-}
-
-func programHandler(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("SELECT id, titre, utilisateur_id FROM topic")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	var topics []Topic
-	for rows.Next() {
-		var t Topic
-		if err := rows.Scan(&t.ID, &t.Titre, &t.UtilisateurID); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		topics = append(topics, t)
-	}
-
-	if err := rows.Err(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	tmpl, err := template.ParseFiles("../front/programme.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, topics)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
