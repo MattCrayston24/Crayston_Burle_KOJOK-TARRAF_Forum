e371abf70edbf0ccdf17215cae182b58bace63f5
Lucas8400 <lucas.burle@ynov.com>
2023-06-20 21:44:28 +0200

    session connexion

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"html/template"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/sessions"
 )
 
-var db *sql.DB
+var (
+	db    *sql.DB
+	store = sessions.NewCookieStore([]byte("secret-key"))
+)
 
 type Utilisateur struct {
 	ID              int
@@ -40,12 +44,27 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "../front/index.html")
+	session, _ := store.Get(r, "session")
+	user := getUser(session.Values["user"])
+
+	tmpl, err := template.ParseFiles("../front/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, "session")
+
 	if r.Method == "GET" {
-		http.ServeFile(w, r, "../front/connexion.html")
+		tmpl, _ := template.ParseFiles("../front/connexion.html")
+		tmpl.Execute(w, nil)
 	} else if r.Method == "POST" {
 		r.ParseForm()
 
@@ -55,13 +74,15 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		var utilisateur Utilisateur
 		err := db.QueryRow("SELECT * FROM utilisateur WHERE adresse_mail = ? AND mot_de_passe = ?", email, password).Scan(&utilisateur.ID, &utilisateur.NomUtilisateur, &utilisateur.AdresseMail, &utilisateur.MotDePasse, &utilisateur.RoleID, &utilisateur.DateInscription)
 		if err != nil {
-			fmt.Fprintln(w, "Connexion échouée")
+			http.Redirect(w, r, "/connexion", http.StatusSeeOther)
 		}
 
 		if utilisateur.AdresseMail != "" {
+			session.Values["user"] = utilisateur.NomUtilisateur
+			session.Save(r, w)
 			http.Redirect(w, r, "/", http.StatusSeeOther)
 		} else {
-			fmt.Fprintln(w, "Connexion échouée")
+			http.Redirect(w, r, "/connexion", http.StatusSeeOther)
 		}
 
 		username := r.FormValue("username")
@@ -72,10 +93,22 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		if username != "" && signupEmail != "" && signupPassword != "" {
 			_, err := db.Exec("INSERT INTO utilisateur (nom_utilisateur, adresse_mail, mot_de_passe, role_id) VALUES (?, ?, ?, ?)", username, signupEmail, signupPassword, roleID)
 			if err != nil {
-				fmt.Fprintln(w, "Inscription échouée")
+				http.Redirect(w, r, "/connexion", http.StatusSeeOther)
 			} else {
+				session.Values["user"] = username
+				session.Save(r, w)
 				http.Redirect(w, r, "/", http.StatusSeeOther)
 			}
 		}
 	}
+}
+
+func getUser(username interface{}) *Utilisateur {
+	if username == nil {
+		return &Utilisateur{}
+	}
+	var utilisateur Utilisateur
+	db.QueryRow("SELECT * FROM utilisateur WHERE nom_utilisateur = ?", username).Scan(&utilisateur.ID, &utilisateur.NomUtilisateur, &utilisateur.AdresseMail, &utilisateur.MotDePasse, &utilisateur.RoleID, &utilisateur.DateInscription)
+
+	return &utilisateur
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module module
 
 go 1.20
 
-require (
-	github.com/go-sql-driver/mysql v1.7.1
-)
+require github.com/go-sql-driver/mysql v1.7.1
 
+require (
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+)
